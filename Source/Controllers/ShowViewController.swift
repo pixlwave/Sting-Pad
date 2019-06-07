91a0ab60fbfb52b0e10645f8e7b1189935a7e78b
Doug <git@pixlwave.uk>
2019-06-07 09:12:07 +0100

    Add editing of sting order and deleting stings to show view controller. Doesn't save the changes yet.

--- Source/Controllers/ShowViewController.swift
+++ Source/Controllers/ShowViewController.swift
@@ -5,6 +5,8 @@ class ShowViewController: UITableViewController {
     private let engine = Engine.shared
     private var editedIndexPath: IndexPath?
     
+    @IBOutlet weak var editBarButtonItem: UIBarButtonItem!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -23,9 +25,13 @@ class ShowViewController: UITableViewController {
         }
     }
     
-    @IBAction func addSting(_ sender: Any) {
+    @IBAction func addSting() {
         engine.addSting()
         tableView.insertRows(at: [IndexPath(row: engine.stings.count - 1, section: 1)], with: .automatic)
+    }
+    
+    @IBAction func edit() {
+        tableView.setEditing(!tableView.isEditing, animated: true)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -48,7 +54,7 @@ class ShowViewController: UITableViewController {
         case 0:
             return 1
         case 1:
-            return engine.stings.count
+            return engine.stings.count + 1
         default:
             return 0
         }
@@ -59,10 +65,14 @@ class ShowViewController: UITableViewController {
         case 0:
             return tableView.dequeueReusableCell(withIdentifier: "File Cell") ?? UITableViewCell()
         case 1:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Edit Sting Cell") ?? UITableViewCell()
-            cell.textLabel?.text = engine.stings[indexPath.row].title
-        
-            return cell
+            if indexPath.row < engine.stings.count {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Edit Sting Cell") ?? UITableViewCell()
+                cell.textLabel?.text = engine.stings[indexPath.row].title
+            
+                return cell
+            } else {
+                return tableView.dequeueReusableCell(withIdentifier: "Add Sting Cell") ?? UITableViewCell()
+            }
         default:
             return UITableViewCell()
         }
@@ -81,9 +91,34 @@ class ShowViewController: UITableViewController {
                 tableView.reloadSections(IndexSet(integer: 1), with: .automatic)
             })
             present(alert, animated: true)
+        case 1:
+            if indexPath.row == engine.stings.count {
+                tableView.deselectRow(at: indexPath, animated: true)
+                addSting()
+            }
         default:
             return
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        guard indexPath.section == 1 else { return false }
+        return indexPath.row < engine.stings.count  // ignores add sting button
+    }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        guard indexPath.section == 1 else { return false }
+        return indexPath.row < engine.stings.count  // ignores add sting button
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { super.tableView(tableView, commit: editingStyle, forRowAt: indexPath); return }
+        engine.stings.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        engine.stings.insert(engine.stings.remove(at: sourceIndexPath.row), at: destinationIndexPath.row)
     }
     
 }
