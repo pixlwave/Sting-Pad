7b9c38fac2f86089242c9efd903ac9d062b7de2a
Doug <git@pixlwave.uk>
2019-06-19 15:32:55 +0100

    Replace Load Track button with Rename button in StingViewController.

--- Source/Controllers/StingViewController.swift
+++ Source/Controllers/StingViewController.swift
@@ -10,7 +10,7 @@ class StingViewController: UIViewController {
     
     @IBOutlet weak var stingNumberLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var waveformLoadingImageView: UIImageView!
     
     override func viewDidLoad() {
@@ -18,8 +18,7 @@ class StingViewController: UIViewController {
         
         // load track info
         stingNumberLabel.text = "Sting \(stingIndex + 1)"
-        titleLabel.text = engine.show.stings[stingIndex].songTitle
-        artistLabel.text = engine.show.stings[stingIndex].songArtist
+        updateLabels()
         
         // set up the waveform view
         waveformView = FDWaveformView(frame: .zero)
@@ -37,6 +36,35 @@ class StingViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         waveformView.frame = waveformLoadingImageView.frame
+    }
+    
+    func updateLabels() {
+        let sting = engine.show.stings[stingIndex]
+        
+        if let name = sting.name {
+            titleLabel.text = name
+            subtitleLabel.text = sting.songTitle
+        } else {
+            titleLabel.text = sting.songTitle
+            subtitleLabel.text = sting.songArtist
+        }
+    }
+    
+    @IBAction func rename() {
+        let sting = engine.show.stings[stingIndex]
+        
+        let alertController = UIAlertController(title: "Rename", message: nil, preferredStyle: .alert)
+        alertController.addTextField { textField in textField.text = sting.name }
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            guard let name = alertController.textFields?.first?.text else { return }
+            sting.name = name.isEmpty == false ? name : nil
+            self.engine.show.updateChangeCount(.done)
+            self.updateLabels()
+            
+            NotificationCenter.default.post(Notification(name: .stingsDidChange))
+        }))
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func zoomWaveOut() {
