292fe551ce2a9b552e33bc4be76e2e19599cc69e
Doug <git@pixlwave.uk>
2019-06-30 17:13:06 +0100

    Implement diffable data source and compositional layout in PlaybackViewController.

--- Source/Controllers/PlaybackViewController.swift
+++ Source/Controllers/PlaybackViewController.swift
@@ -4,6 +4,7 @@ import MediaPlayer
 class PlaybackViewController: UICollectionViewController {
     
     private let engine = Engine.shared
+    lazy private var dataSource = makeDataSource()
     private var cuedStingIndex = 0
     
     @IBOutlet var transportView: UIView!
@@ -23,6 +24,8 @@ class PlaybackViewController: UICollectionViewController {
         view.addSubview(transportView)
         timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize, weight: .regular)
         
+        collectionView.dataSource = dataSource
+        collectionView.collectionViewLayout = createLayout()
         // prevents scroll view from momentarily blocking the play button's action
         collectionView.delaysContentTouches = false; #warning("Test if this works or if the property needs to be set on the scroll view")
         
@@ -44,9 +47,44 @@ class PlaybackViewController: UICollectionViewController {
         collectionView.contentInset.bottom = size.height
     }
     
+    func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+            let minimumWidth: CGFloat = 300
+            let count = Int(layoutEnvironment.container.effectiveContentSize.width / minimumWidth)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: count)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = item.contentInsets
+            
+            return section
+        }
+    }
+    
+    func makeDataSource() -> UICollectionViewDiffableDataSource<Int, Sting> {
+        return UICollectionViewDiffableDataSource<Int, Sting>(collectionView: collectionView) { collectionView, indexPath, sting -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Sting Cell", for: indexPath)
+            
+            guard let stingCell = cell as? StingCell else { return cell }
+            
+            let sting = self.engine.show.stings[indexPath.item]
+            stingCell.titleLabel.text = sting.name ?? sting.songTitle
+            stingCell.isCued = indexPath.item == self.cuedStingIndex
+            
+            return stingCell
+        }
+    }
+    
     #warning("Implement more efficient responses to changed data.")
     @objc func reloadData() {
-        collectionView.reloadData()
+        let snapshot = NSDiffableDataSourceSnapshot<Int, Sting>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(engine.show.stings)
+        dataSource.apply(snapshot)
     }
     
     func showWelcomeScreen() {
@@ -110,23 +148,7 @@ class PlaybackViewController: UICollectionViewController {
     }
     
     
-    // MARK: UICollectionViewDataSource/Delegate
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return engine.show.stings.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Sting Cell", for: indexPath)
-        
-        guard let stingCell = cell as? StingCell else { return cell }
-        
-        let sting = engine.show.stings[indexPath.item]
-        stingCell.titleLabel.text = sting.name ?? sting.songTitle
-        stingCell.isCued = indexPath.item == cuedStingIndex
-        
-        return stingCell
-    }
-    
+    // MARK: UICollectionViewDataDelegate
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         engine.playSting(at: indexPath.item)
     }
