8a225fb0536250c7b677348a03eb47ea5a5c8e59
Doug <git@pixlwave.uk>
2019-07-21 12:54:29 +0100

    Add document browser key to Info.plist. Add open url method to app delegate with warning to implement this in the future (returns false for now).

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        #warning("Missing implementation for opening urls")
+        return false
+    }
+    
     func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
         return true
     }
