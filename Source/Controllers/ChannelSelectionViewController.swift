3ba93d2ecd2173b635e8aae43a477a6917acb5f2
Doug <git@pixlwave.uk>
2019-08-04 10:58:05 +0100

    Replace individual left & right configs with paired output config.

--- Source/Controllers/ChannelSelectionViewController.swift
+++ Source/Controllers/ChannelSelectionViewController.swift
@@ -3,43 +3,48 @@ import AVFoundation
 
 class ChannelSelectionViewController: UITableViewController {
     
-    enum PlaybackChannel {
-        case left, right
-    }
-    var playbackChannel: PlaybackChannel = .left
     var outputConfig = Engine.shared.outputConfig
     let outputChannelCount = Engine.shared.outputChannelCount()
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return outputChannelCount
+        return outputChannelCount / 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        #warning("Get name of audio interface here...")
+        return "Audio Interface Name"
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChannelCell") ?? UITableViewCell()
         
-        cell.textLabel?.text = "Output \(indexPath.row + 1)"
+        let cellChannels = channels(for: indexPath)
         
-        switch playbackChannel {
-        case .left:
-            cell.accessoryType = outputConfig.left == indexPath.row ? .checkmark : .none
-        case .right:
-            cell.accessoryType = outputConfig.right == indexPath.row ? .checkmark : .none
+        if let textLabel = cell.textLabel {
+            textLabel.text = "Channels \(cellChannels.0 + 1) & \(cellChannels.1 + 1)"
+            textLabel.font = UIFont.monospacedDigitSystemFont(ofSize: textLabel.font.pointSize, weight: .regular)
+        }
+        
+        if outputConfig.left == cellChannels.0, outputConfig.right == cellChannels.1 {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
         }
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch playbackChannel {
-        case .left:
-            outputConfig.left = indexPath.row
-            tableView.reloadData()
-        case .right:
-            outputConfig.right = indexPath.row
-            tableView.reloadData()
-        }
+        let selectedChannels = channels(for: indexPath)
+        outputConfig.left = selectedChannels.0
+        outputConfig.right = selectedChannels.1
+        tableView.reloadData()
         
         Engine.shared.outputConfig = outputConfig
+    }
+    
+    func channels(for indexPath: IndexPath) -> (Int, Int) {
+        return ((2 * indexPath.row), (2 * indexPath.row) + 1)
     }
     
 }
--- Source/Controllers/SettingsViewController.swift
+++ Source/Controllers/SettingsViewController.swift
@@ -2,16 +2,6 @@ import UIKit
 
 class SettingsViewController: UITableViewController {
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "Left Channel Selection", let channelSelectionVC = segue.destination as? ChannelSelectionViewController {
-            channelSelectionVC.playbackChannel = .left
-            channelSelectionVC.navigationItem.title = "Left Channel"
-        } else if segue.identifier == "Right Channel Selection", let channelSelectionVC = segue.destination as? ChannelSelectionViewController {
-            channelSelectionVC.playbackChannel = .right
-            channelSelectionVC.navigationItem.title = "Right Channel"
-        }
-    }
-    
     @IBAction func done(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
