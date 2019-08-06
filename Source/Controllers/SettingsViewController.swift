689604a18098f67f80300c9ebf945666f457498b
Doug <git@pixlwave.uk>
2019-08-06 21:41:23 +0100

    Add a button to import music files into the app (unimplemented).

--- Source/Controllers/SettingsViewController.swift
+++ Source/Controllers/SettingsViewController.swift
@@ -5,4 +5,15 @@ class SettingsViewController: UITableViewController {
     @IBAction func done(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
+    
+    func importMusic() {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.section == 0 else { super.tableView(tableView, didSelectRowAt: indexPath); return }
+        
+        importMusic()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
