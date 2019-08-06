550c3304ee9be55c136764b9b08993d132f0e63b
Doug <git@pixlwave.uk>
2019-08-06 22:52:48 +0100

    First attempt at styling the AddStingFooterView.

---
+++ Source/Views/DashedBorderView.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class DashedBorderView: UIView {
+    
+    let shape = CAShapeLayer()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeColor = UIColor.secondaryLabel.cgColor
+        shape.lineWidth = 4
+        shape.lineDashPattern = [8, 8]
+        layer.addSublayer(shape)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        shape.path = CGPath(roundedRect: bounds, cornerWidth: 8, cornerHeight: 8, transform: nil)
+    }
+    
+}
