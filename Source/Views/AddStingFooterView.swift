9ce58086d188f17f36d1964eafbc573d1e334b7b
Doug <git@pixlwave.uk>
2019-08-06 12:38:17 +0100

    Fix function name.

--- Source/Views/AddStingFooterView.swift
+++ Source/Views/AddStingFooterView.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class AddStingFooterView: UICollectionReusableView {
     
-    @IBAction func AddFromLibrary() {
+    @IBAction func addFromLibrary() {
         #warning("Is this necessary or an SDK bug workaround")
         // send a notification as wiring up an action on the file's owner only worked once
         NotificationCenter.default.post(Notification(name: .addStingFromLibrary))
