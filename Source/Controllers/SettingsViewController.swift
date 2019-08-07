aeb92037b37c7db32b6caf49f8cd9a29c2348589
Doug <git@pixlwave.uk>
2019-08-07 12:07:48 +0100

    Remove the Import Music button as this hopefully won't be necessary 🤞🏻.

--- Source/Controllers/SettingsViewController.swift
+++ Source/Controllers/SettingsViewController.swift
@@ -6,14 +6,4 @@ class SettingsViewController: UITableViewController {
         dismiss(animated: true)
     }
     
-    func importMusic() {
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard indexPath.section == 0 else { super.tableView(tableView, didSelectRowAt: indexPath); return }
-        
-        importMusic()
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
 }
