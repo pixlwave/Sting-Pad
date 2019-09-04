98a382d22da394b1cfa58b75a8effea82bb3d85e
Doug <git@pixlwave.uk>
2019-09-04 16:15:17 +0100

    Prevent swipe down to dismiss modal gesture from interfering with waveform gestures.

--- FDWaveformView/FDWaveformView.swift
+++ FDWaveformView/FDWaveformView.swift
@@ -500,7 +500,7 @@ enum FDWaveformType: Equatable {
 
 extension FDWaveformView: UIGestureRecognizerDelegate {
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
+        return gestureRecognizers?.contains(otherGestureRecognizer) ?? false
     }
 
     @objc func handlePinchGesture(_ recognizer: UIPinchGestureRecognizer) {
