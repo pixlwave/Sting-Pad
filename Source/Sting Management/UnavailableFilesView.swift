38159866bf3540123508ef4f8fd213580e46a9d3
Doug <git@pixlwave.uk>
2025-09-06 11:34:16 +0100

    Use a safeAreaBar for Sting management headers.

--- Source/Sting Management/UnavailableFilesView.swift
+++ Source/Sting Management/UnavailableFilesView.swift
@@ -7,23 +7,25 @@ struct UnavailableFilesView: View {
     @State var isPresentingFolderPicker = false
     
     var body: some View {
-        VStack {
-            Text("Sting Pad is unable to access the following files on this device. Ensure they are downloaded in the Files app and grant access to the containing folder to load your stings.")
-                .font(.headline)
-                .padding(.horizontal)
-                .padding(.bottom, 1)
-            Button ("Access Folder") { isPresentingFolderPicker = true }
-                .font(.body.bold())
-                .padding(.bottom, 8)
-            
-            List(stings, id: \.self) { sting in
-                UnavailableStingCell(sting: sting, show: show)
+        List(stings, id: \.self) { sting in
+            UnavailableStingCell(sting: sting, show: show)
+        }
+        .safeAreaBar(edge: .top) {
+            VStack {
+                Text("Sting Pad is unable to access the following files on this device. Ensure they are downloaded in the Files app and grant access to the containing folder to load your stings.")
+                    .font(.subheadline)
+                    .padding(.horizontal)
+                    .padding(.bottom, 1)
+                Button ("Access Folder") { isPresentingFolderPicker = true }
+                    .font(.body.bold())
             }
-            .overlay(alignment: .top) { Divider() }
         }
         .navigationTitle("Manage Permissions")
+        .navigationBarTitleDisplayMode(.inline)
+        .interactiveDismissDisabled()
         .sheet(isPresented: $isPresentingFolderPicker) {
             FolderAccessView(show: show)
+                .presentationSizing(.fitted)
         }
     }
 }
--- Source/Sting Management/UnavailableSongsView.swift
+++ Source/Sting Management/UnavailableSongsView.swift
@@ -5,20 +5,21 @@ struct UnavailableSongsView: View {
     @Binding var stings: [Sting]
     
     var body: some View {
-        VStack {
-            Text("Sting Pad is unable to locate the following songs on your device. Ensure that they are downloaded in the Music app and reload the show.")
-                .font(.headline)
-                .padding(.horizontal)
-                .padding(.bottom, 1)
-            Button("Reload Stings", action: show.reloadUnavailableStings)
-                .font(.body.bold())
-                .padding(.bottom, 8)
-            
-            List(stings, id: \.self) { sting in
-                UnavailableStingCell(sting: sting, show: show)
+        List(stings, id: \.self) { sting in
+            UnavailableStingCell(sting: sting, show: show)
+        }
+        .safeAreaBar(edge: .top) {
+            VStack {
+                Text("Sting Pad is unable to locate the following songs on your device. Ensure that they are downloaded in the Music app and reload the show.")
+                    .font(.headline)
+                    .padding(.horizontal)
+                    .padding(.bottom, 1)
+                Button("Reload Stings", action: show.reloadUnavailableStings)
+                    .font(.body.bold())
             }
-            .overlay(alignment: .top) { Divider() }
         }
         .navigationTitle("Missing Strings")
+        .navigationBarTitleDisplayMode(.inline)
+        .interactiveDismissDisabled()
     }
 }
