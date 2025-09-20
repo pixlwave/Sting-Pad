2788058dccb24dae16945fbe8cb709b786b8a207
Doug <git@pixlwave.uk>
2025-09-20 15:44:01 +0100

    Use a glass on the WelcomeView.

--- Source/Help/WelcomeView.swift
+++ Source/Help/WelcomeView.swift
@@ -6,40 +6,43 @@ struct WelcomeView: View {
     static let currentVersion = 3.0
     
     var body: some View {
+        ScrollView {
+            content
+                .padding(20)
+        }
+        .safeAreaBar(edge: .bottom) {
+            Button(action: dismiss.callAsFunction) {
+                Text("Continue")
+                    .font(.headline)
+                    .frame(maxWidth: 300)
+            }
+            .buttonStyle(.glassProminent)
+            .controlSize(.large)
+            .padding()
+        }
+    }
+    
+    var content: some View {
         VStack {
-            Spacer()
-            
             Text("Getting Started")
                 .font(.largeTitle.bold())
+                .padding()
+                .padding(.bottom)
             
-            Spacer()
-            
-            VStack(alignment: .leading, spacing: 25) {
-                WelcomeItem(symbolName: "plus.circle.fill",
+            VStack(alignment: .leading, spacing: 20) {
+                WelcomeItem(symbolName: "plus.circle",
                             symbolColor: .systemPurple,
                             text: "Tap + to start a new show.")
-                WelcomeItem(symbolName: "folder.circle.fill",
+                WelcomeItem(symbolName: "folder.circle",
                             symbolColor: .systemBlue,
                             text: "Add stings from your device.")
-                WelcomeItem(symbolName: "waveform.circle.fill",
+                WelcomeItem(symbolName: "waveform.circle",
                             symbolColor: .systemYellow,
                             text: "Long tap on a sting to modify it.")
-                WelcomeItem(symbolName: "arrow.up.arrow.down.circle.fill",
+                WelcomeItem(symbolName: "arrow.up.arrow.down.circle",
                             symbolColor: .systemGreen,
                             text: "Long tap and drag to move a sting.")
             }
-            .padding(20)
-            
-            Spacer()
-            Spacer()
-            
-            Button("Continue", action: dismiss.callAsFunction)
-                .buttonStyle(.borderedProminent)
-                .controlSize(.large)
-                .font(.headline)
-                .tint(.tint)
-            
-            Spacer()
         }
     }
 }
@@ -52,8 +55,9 @@ struct WelcomeItem: View {
     var body: some View {
         HStack {
             Image(systemName: symbolName)
-                .font(.system(size: 36))
+                .font(.largeTitle.scaled(by: 1.1))
                 .foregroundColor(Color(symbolColor))
+                .symbolVariant(.fill)
                 .padding(.trailing, 10)
             Text(text)
         }
